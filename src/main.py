4be25edd14722c49b63e51f1ca9278daa6d2c3a7
Borroot <borroot@live.nl>
2020-11-19 19:44:54 +0100

    Rename left and right to west and east.

--- src/main.py
+++ src/main.py
@@ -14,27 +14,27 @@ def init_draw():
     pygame.init()
     pygame.display.set_caption("Wolfenstein")
 
-    surf_root  = pygame.display.set_mode(SIZE_WHOLE)
-    surf_left  = pygame.Surface(SIZE_LEFT)
-    surf_right = pygame.Surface(SIZE_RIGHT)
-    return surf_root, surf_left, surf_right
+    surf_root = pygame.display.set_mode(SIZE_WHOLE)
+    surf_west = pygame.Surface(SIZE_WEST)
+    surf_east = pygame.Surface(SIZE_EAST)
+    return surf_root, surf_west, surf_east
 
 
 def loop_draw():
     global alive, world
-    surf_root, surf_left, surf_right = init_draw()
+    surf_root, surf_west, surf_east = init_draw()
 
     clock = pygame.time.Clock()
     while alive:
         clock.tick(FPS)
 
-        surf_left.fill(pygame.Color('white'))
-        surf_right.fill(pygame.Color('white'))
+        surf_west.fill(pygame.Color('white'))
+        surf_east.fill(pygame.Color('white'))
 
-        world.draw(surf_left)
+        world.draw(surf_west)
 
-        surf_root.blit(surf_left,  (  0, 0))
-        surf_root.blit(surf_right, (800, 0))
+        surf_root.blit(surf_west, PLACE_WEST)
+        surf_root.blit(surf_east, PLACE_EAST)
         pygame.display.update()
 
     pygame.quit()
