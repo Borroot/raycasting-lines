27316a851679332bf1e68a0303244a0372a63754
Borroot <borroot@live.nl>
2020-11-30 11:18:38 +0100

    Add a maze generator.

--- src/level.py
+++ src/level.py
@@ -1,40 +1,55 @@
-from wall import Wall
-from maze import Maze
+import random
+
+from maze import Maze, EMPTY
+
+
+"""
+Generate a maze using depth-first search recursive backtracking.
+
+The algorithm is implemented as follows:
+    1. Make the initial cell the current cell and mark it as visited
+    2. While there are unvisited cells
+        1. If the current cell has any neighbours which have not been visited
+            1. Choose randomly one of the unvisited neighbours
+            2. Push the current cell to the stack
+            3. Remove the wall between the current cell and the chosen cell
+            4. Make the chosen cell the current cell and mark it as visited
+        2. Else if the visited stack is not empty
+            1. Pop a cell from the visited stack
+            2. Make it the current cell
+"""
+
+
+def neighbours_unvisited(maze, current, unvisited):
+    neighbours = []
+    for offset in [(-2, 0), (0, -2), (2, 0), (0, 2)]:
+        neighbour = (current[0] + offset[0], current[1] + offset[1])
+        if neighbour in unvisited:
+            neighbours.append(neighbour)
+    return neighbours
+
+
+def remove_wall(maze, cell1, cell2):
+    maze.grid[(cell1[1] + cell2[1]) // 2][(cell1[0] + cell2[0]) // 2] = EMPTY
 
 
 def generate(width, height):
     maze = Maze(width, height)
+    visited = []
+    unvisited = set(maze.cells())
 
-    # TODO Generate a maze using the backtrack algorithm.
+    current = unvisited.pop()
+    visited.append(current)
+
+    while len(unvisited) > 0:
+        if len(neighbours:=neighbours_unvisited(maze, current, unvisited)) > 0:
+            neighbour = random.choice(neighbours)
+            remove_wall(maze, current, neighbour)
+
+            current = neighbour
+            unvisited.remove(current)
+            visited.append(current)
+        elif len(visited) > 0:
+            current = visited.pop()
 
     return maze
-
-
-
-LEVEL1 = [
-    Wall((200, 200), (350, 200)),
-    Wall((450, 200), (600, 200)),
-    Wall((200, 200), (200, 300)),
-    Wall((600, 200), (600, 300)),
-    Wall((200, 300), (600, 300)),
-    Wall((200, 300), (200, 500)),
-    Wall((200, 500), (200, 600)),
-    Wall((600, 300), (600, 600)),
-    Wall((200, 600), (350, 600)),
-    Wall((450, 600), (600, 600)),
-    Wall((350, 600), (200, 500))]
-
-
-LEVEL2 = [
-    Wall((200, 100), (300, 100)),
-    Wall((300, 100), (300, 200)),
-    Wall((300, 200), (200, 200)),
-    Wall((200, 200), (200, 100)),
-    Wall((200, 200), (200, 300)),
-    Wall((200, 300), (100, 300)),
-    Wall((100, 300), (100, 200)),
-    Wall((100, 200), (200, 200)),
-    Wall((400, 300), (600, 300)),
-    Wall((600, 300), (600, 400)),
-    Wall((600, 400), (400, 400)),
-    Wall((400, 400), (400, 300))]
--- src/maze.py
+++ src/maze.py
@@ -1,11 +1,48 @@
-import pygame
+EMPTY = 0
+WALL = 1
 
 
 class Maze:
 
     def __init__(self, width, height):
-        pass
+        self.grid = [[WALL if x % 2 == 0 or y % 2 == 0 else EMPTY
+            for x in range(2*width +1)] for y in range(2*height +1)]
 
 
-    def draw(self, surface):
+    def __str__(self):
+        builder = ''
+        for y in range(len(self.grid)):
+            for x in range(len(self.grid[y])):
+                if self.grid[y][x] == EMPTY:
+                    builder += ' '
+                else:  # maze[y][x] == WALL
+                    if y % 2 == 0:
+                        if x % 2 == 0:
+                            builder += '+'
+                        else:
+                            builder += '-'
+                    else:
+                        builder += '|'
+                builder += ' '
+            if y < len(self.grid) - 1:
+                builder += '\n'
+        return builder
+
+
+    def __repr__(self):
+        return self.__str__()
+
+
+    def cells(self):
+        """ Return all the cell coordinates on the grid. """
+        cells = []
+        for y in range(len(self.grid)):
+            for x in range(len(self.grid[y])):
+                if not (x % 2 == 0 or y % 2 == 0):
+                    cells.append((x, y))
+        return cells
+
+
+    def walls(self):
+        """ Return all the walls which can be used for drawing the maze. """
         pass
